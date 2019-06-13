77ecf998b3af07bcd3ffaaae0af21c9bd867b140
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2019-06-13 13:14:26 +0100

    added jsonbin service

---
+++ src/models/ProconBin.ts
@@ -0,0 +1,7 @@
+import { Reason } from "./Reason";
+
+export interface ProconBin {
+  id: string;
+  pros: Reason[],
+  cons: Reason[]
+}
---
+++ src/services/JsonBin.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { ProconBin } from '../models/ProconBin';
+
+const instance = axios.create({
+  baseURL: 'https://api.jsonbin.io/',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    'secret-key': process.env.REACT_APP_API_BIN_KEY,
+    'collection-id': '5d00d8bec8ef78426778efff'
+  }
+});
+
+export async function getBin(id: string) {
+  const response = await instance.get(`b/${id}`);
+  return response.data as ProconBin;
+}
